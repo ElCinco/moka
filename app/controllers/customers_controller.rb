dcfa2ff302cf42f3425cd32c0e302de60bb0d96e
flanban <johnsfuller@gmail.com>
2017-07-13 13:40:04 -0400

    tweaking form markup, wip

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,6 @@
 class CustomersController < ApplicationController
   def create
-    @customer = Customer.new(params.require(:customer).permit(:age, :gender, :email, :phone_number, :state, :smoker))
+    @customer = Customer.new(params.require(:customer).permit(:age, :gender, :email, :phone_number, :state, :smoker, :coverage_amount, :customer_name, :weight, :height, :marriage_status, :children, :income_range, :health_rating, :currently_has_policy))
 
     @customer.save
     if @customer.save
